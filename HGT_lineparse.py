690ad03fd44fcdab129f7a377525298ad54c9573
Derreckadam <dcart001@ucr.edu>
2016-02-19 16:20:43 -0800

    Update HGT_lineparse.py

--- HGT_lineparse.py
+++ HGT_lineparse.py
@@ -2,11 +2,11 @@
 import csv
 import sys
 
-filename = sys.argv[1]
+hgttxt = sys.argv[1]
 
-print ("input file: ", filename)
+print ("input file: ", hgttxt)
 
-with open(filename, 'rb') as csvfile:
+with open(hgttxt, 'rb') as csvfile:
 	reader = csv.reader(csvfile)
 	for row in reader:
 		print (row[0], row[1], row[2])
