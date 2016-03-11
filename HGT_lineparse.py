479dbbbb8cc8bcfb12f1f95694d0131e83d79120
Derreckadam <dcart001@ucr.edu>
2016-03-11 13:18:46 -0800

    Update HGT_lineparse.py

--- HGT_lineparse.py
+++ HGT_lineparse.py
@@ -1,12 +1,15 @@
-# HGT_lineparse
+#! /usr/bin/python
+# HGT_lineparse by Derreck
 import csv
 import sys
 
 hgttxt = sys.argv[1]
 
-print ("input file: ", hgttxt)
+print ("input file: " + hgttxt)
 
 with open(hgttxt, 'rb') as csvfile:
 	reader = csv.reader(csvfile)
 	for row in reader:
-		print (row[0], row[1], row[2])
+		print (row[0], float(row[1]), row[2])
+		if float(row[1]) <= .0000000000000000001:
+			print row[1]
