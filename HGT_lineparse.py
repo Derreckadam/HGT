de07e287b96aa06bd6d7cc4120b279bdba57a899
Derreckadam <dcart001@ucr.edu>
2016-03-11 16:02:23 -0800

    Update HGT_lineparse.py

--- HGT_lineparse.py
+++ HGT_lineparse.py
@@ -9,7 +9,13 @@ print ("input file: " + hgttxt)
 
 with open(hgttxt, 'rb') as csvfile:
 	reader = csv.reader(csvfile)
+	count = 0
 	for row in reader:
+		count += 1
 		print (row[0], float(row[1]), row[2])
 		if float(row[1]) <= .0000000000000000001:
 			print row[1]
+			print count
+			hits = open("HGTFILE.txt", "w")
+			hits.write("Row number: " + str(count) + row[1] + "\n")
+			hits.close()
